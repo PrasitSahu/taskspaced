68165ee219166b8536845b056a3e55eab5474c68
Prasit Sahu <prasitsahu4@gmail.com>
2024-11-05 08:24:24 +0530

    added contact us page

---
+++ app/contact/page.tsx
@@ -0,0 +1,97 @@
+import { Button } from "@/components/ui/button";
+import Header from "../Header";
+import Footer from "../Footer";
+import { Input } from "@/components/ui/input";
+import { Link } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
+
+const ContactPage = () => {
+  return (
+    <>
+      <Header />
+      <section className="px-10">
+        <div className="container px-5 py-6 mx-auto flex sm:flex-nowrap flex-wrap">
+          <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+            <iframe
+              width="100%"
+              height="100%"
+              title="map"
+              className="absolute inset-0"
+              src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
+            ></iframe>
+            <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+              <div className="lg:w-1/2 px-6">
+                <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                  ADDRESS
+                </h2>
+                <p className="mt-1">
+                  Photo booth tattooed prism, portland taiyaki hoodie neutra
+                  typewriter
+                </p>
+              </div>
+              <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                  EMAIL
+                </h2>
+                <Link
+                  className="text-indigo-400 leading-relaxed"
+                  href="mailto:example@email.com"
+                >
+                  example@email.com
+                </Link>
+                <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                  PHONE
+                </h2>
+                <p className="leading-relaxed">123-456-7890</p>
+              </div>
+            </div>
+          </div>
+          <div className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+            <h2 className="text-white text-lg mb-1 font-medium title-font">
+              Contact Us
+            </h2>
+            <p className="leading-relaxed mb-5">
+              Post-ironic portland shabby chic echo park, banjo fashion axe
+            </p>
+            <div className="relative mb-4">
+              <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+                Name
+              </label>
+              <Input type="text" id="name" name="name" />
+            </div>
+            <div className="relative mb-4">
+              <label
+                htmlFor="email"
+                className="leading-7 text-sm text-gray-400"
+              >
+                Email
+              </label>
+              <Input type="email" id="email" name="email" />
+            </div>
+            <div className="relative mb-4">
+              <label
+                htmlFor="message"
+                className="leading-7 text-sm text-gray-400"
+              >
+                Message
+              </label>
+              <Textarea
+                id="message"
+                name="message"
+                className="w-full bg-background rounded h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none"
+              ></Textarea>
+            </div>
+            <Button>Send Message</Button>
+            <p className="text-xs text-gray-400 text-opacity-90 mt-3">
+              Chicharrones blog helvetica normcore iceland tousled brook viral
+              artisan.
+            </p>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+};
+
+export default ContactPage;
